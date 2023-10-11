b4ba956539d734d8bbfa9fa2852756fc6b53ebd9
gabrielts212 <gabrielts2122@gmail.com>
2023-10-11 18:16:42 -0300

    api marcando endereço no mapa ✔

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,20 +1,16 @@
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, Marker, useJsApiLoader, LoadScript, StandaloneSearchBox } from "@react-google-maps/api";
 import React, { useState } from "react";
-import { LoadScript, StandaloneSearchBox } from "@react-google-maps/api";
-
-import  styles from "./../../styles/api.module.css";
+import styles from "./../../styles/api.module.css";
 
 const MapPage = () => {
-
-
   const [map, setMap] = useState(null);
   const [searchBox, setSearchBox] = useState(null);
   const [markers, setMarkers] = useState([]);
+  const [center, setCenter] = useState({
+    lat: -22.9068,
+    lng: -43.1729
+  });
 
-  const position = {
-    lat: -27.590824,
-    lng: -48.551262,
-  };
   const onMapLoad = (map) => {
     setMap(map);
   };
@@ -23,70 +19,51 @@ const MapPage = () => {
     setSearchBox(ref);
   };
 
-  // const onPlacesChanged = () => {
-  //   if (searchBox) {
-  //     const places = searchBox.getPlaces();
-  //     console.log(places);
-  //     if (places.length > 0) {
-  //       const place = places[0];
-  //       const location = {
-  //         lat: place.geometry.location.lat() || 0,
-  //         lng: place.geometry.location.lng() || 0,
-  //       };
-  //       setMarkers([...markers, location]);
-  //       map.panTo(location);
-  //     }
-  //   }
-  // };
   const onPlacesChanged = () => {
-    const places = searchBox.getPlaces();
-    console.log(places);
-    const place = places[0];
-    const location = {
-      lat: place?.geometry?.location?.lat() || 0,
-      lng: place?.geometry?.location?.lng() || 0,
-
+    if (searchBox) {
+      const places = searchBox.getPlaces();
+      if (places.length > 0) {
+        const place = places[0];
+        const location = {
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng()
+        };
+        setMarkers([...markers, location]);
+        setCenter(location);
+      }
     }
-    // setMarkers([...markers, location])
-    map?.panTo(location)
   };
 
   return (
-    <div  className={styles.map} >
-      
-    <LoadScript googleMapsApiKey={"AIzaSyCO-vDdf7TyuszVt16uV9xY53uCcBygA5k"} libraries={['places']} >
-      <GoogleMap
-        onLoad={onMapLoad}
-        mapContainerStyle={{ width: "100vh", height: "80vh" }}
-        center={position}
-        zoom={15}
-       
-      >
+    <div className={styles.map}>
+      <LoadScript googleMapsApiKey={"AIzaSyCO-vDdf7TyuszVt16uV9xY53uCcBygA5k"} libraries={['places']}>
+        <GoogleMap
+          onLoad={onMapLoad}
+          mapContainerStyle={{ width: "100%", height: "80vh" }}
+          center={center}
+          zoom={15}
+        >
+          {markers.map((marker, index) => (
+            <Marker key={index} position={marker} />
+          ))}
+
           <Marker
-          className={styles.mapMarker}
-            position={position}
+            className={styles.mapMarker}
+            position={center}
             options={{
               label: {
                 text: 'Posição Teste',
-               
               },
             }}
           />
-          
-        <StandaloneSearchBox onLoad={onLoad} onPlacesChanged={onPlacesChanged}>
-          <input  placeholder="Digite um endereço" className={styles.address}/>
-        </StandaloneSearchBox>
 
-        {markers.map((marker, index) => (
-          <Marker key={index} position={marker} />
-        ))}
-
-
-
-      </GoogleMap>
-    </LoadScript>
-    <></>
-  </div>
+          <StandaloneSearchBox onLoad={onLoad} onPlacesChanged={onPlacesChanged}>
+            <input placeholder="Digite um endereço" className={styles.address} />
+          </StandaloneSearchBox>
+        </GoogleMap>
+      </LoadScript>
+      <></>
+    </div>
   );
 };
 
